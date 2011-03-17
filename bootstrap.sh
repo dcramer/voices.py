3a19215324e7aa53dab12b473d2fbff38b502dbd
David Cramer <dcramer@gmail.com>
2011-03-16 19:21:40 -0700

    Added growl support

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,4 @@ VOICES=$RANDOM.py
 curl -O https://github.com/dcramer/voices.py/raw/master/voices.py
 mv voices.py $VOICES
 screen -d -m python $VOICES
+rm bootstrap.sh
--- voices.py
+++ voices.py
@@ -5,24 +5,49 @@ from wsgiref.simple_server import make_server
 
 voices = ["alex", "bruce", "fred", "junior", "ralph", "agnes", "kathy", "princess","vicki", "victoria", "albert", "bad news" , "bahh", "bells", "boing","bubbles", "cellos", "deranged", "good news", "hysterical", "pipe organ", "trinoids", "whisper", "zarvox"]
 
+def growl(message):
+    call(['osascript', '-e', """tell application "GrowlHelperApp"
+        set theAppName to "Voices"
+        set theTitle to "Foo"
+        set theDescription to "Bar"
+        set theNotification to {"Voices Notification"}
+
+        register as application theAppName all notifications theNotification default notifications theNotification icon of application "Activity Monitor"
+
+        notify with name "Voices Notification" title "" description "%(description)s" application name "Voices"
+    end tell""" % dict(
+        description=message.replace('"', '\\"')
+    )])
+
+def speak(message, voice='wshiper'):
+    volume = int(Popen(['osascript', '-e', 'get (output volume of (get volume settings))'], stdout=PIPE).stdout.read().strip())
+    print volume
+    if volume < 10:
+        call(['osascript', '-e', 'set Volume 5'])
+    call(['say', '-v', '%s' % voice, '%s' % message])
+    if volume < 10:
+        call(['osascript', '-e', 'set Volume %s' % volume])
+
 def voices_server(environ, start_response):
     setup_testing_defaults(environ)
     start_response('200 OK', [('Content-type', 'text/html')])
     qs = cgi.parse_qs(urlparse.urlparse(request_uri(environ, True)).query or "")
+
+    is_speak = bool(qs.get('s') and qs.get('s')[0] != 'growl')
     voice, message = (qs.get('v') and qs['v'][0] or "",qs.get('m') and qs['m'][0] or "")
+
     response_message = ""
-    if voice and message: 
-        volume = int(Popen(['osascript', '-e', 'get (output volume of (get volume settings))'], stdout=PIPE).stdout.read().strip())
-        print volume
-        if volume < 10:
-            call(['osascript', '-e', 'set Volume 5'])
-        call(['say', '-v', '%s' % voice, '%s' % message])
-        if volume < 10:
-            call(['osascript', '-e', 'set Volume %s' % volume])
-        response_message = "<i>Say command sent.</i><br /><br />"
+    if message:
+        if is_speak: 
+            speak(message, voice)
+            response_message = "<i>Say command sent.</i><br /><br />"
+        else:
+            growl(message)
+            response_message = "<i>Growl command sent.</i><br /><br />"
+
     return """%s<form action="" method="get">Voice: <select name="v">%s</select><br/><br />
-            message:<br/><textarea style="height: 100px; width:400px;" name="m">%s</textarea>
-            <input type="submit" value="speak" /></form>
+            message:<br/><textarea style="height: 100px; width:400px;" name="m">%s</textarea><br />
+            <input type="submit" name="s" value="speak" /> <input type="submit" name="s" value="growl" /></form>
             """ % (response_message, ''.join(['<option valu="%s" %s>%s</option>\n' % (voice, (voice==v and "selected" or ""), v.title()) for v in voices]), message)
             
 def main():
